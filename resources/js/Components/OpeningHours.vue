882f59fc2098811a2c64e680d1c452236c96ca2a
Aurelien <aurelien.bouvaist@gmail.com>
2023-04-30 12:34:00 +0200

    simple display for opening hours

---
+++ resources/js/Components/OpeningHours.vue
@@ -0,0 +1,24 @@
+<template>
+    <table>
+        <tr v-for="(hours, day) in openingHours">
+            <td>{{ day }}</td>
+            <td v-for="hour in hours">{{ hour }}</td>
+        </tr>
+    </table>
+</template>
+
+<script setup>
+import {usePage} from "@inertiajs/vue3";
+import {computed} from "vue";
+
+let page = usePage();
+
+const openingHours = computed(() => {
+    return page.props.opening_hours.data.days;
+});
+
+</script>
+
+<style scoped>
+
+</style>
