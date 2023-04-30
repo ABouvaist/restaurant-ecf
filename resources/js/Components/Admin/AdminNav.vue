f6d7d06287f25013e529f430e29604c72e05c70e
Aurelien <aurelien.bouvaist@gmail.com>
2023-04-30 15:21:20 +0200

    add admin layout

---
+++ resources/js/Components/Admin/AdminNav.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav class="pt-4">
+        <ul class="flex justify-around">
+            <li>
+                <Link :href="route('images.index')">Gallerie</Link>
+            </li>
+            <li>Carte</li>
+            <li>Menus</li>
+            <li>Horaires</li>
+        </ul>
+    </nav>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Layouts/AdminLayout.vue
@@ -0,0 +1,17 @@
+<template>
+    <main class="container min-h-screen bg-platinum">
+        <Nav/>
+        <AdminNav/>
+
+        <slot />
+    </main>
+</template>
+
+<script setup>
+import Nav from "@/Components/Nav.vue";
+import AdminNav from "@/Components/Admin/AdminNav.vue"
+</script>
+
+<style scoped>
+
+</style>
