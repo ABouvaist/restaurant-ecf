1187bf140210ae993d55a2ed2d4d69e952087dfd
Aurelien <aurelien.bouvaist@gmail.com>
2023-04-30 12:27:35 +0200

    add attribute for each day

--- app/Models/OpeningHours.php
+++ app/Models/OpeningHours.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use App\Casts\TimeRange;
 use Illuminate\Database\Eloquent\Model;
 
@@ -26,4 +27,46 @@ class OpeningHours extends Model
         'saturday' => TimeRange::class,
         'sunday' => TimeRange::class,
     ];
+
+    private function makeAttribute(): Attribute
+    {
+        return Attribute::make(
+            get: static fn ($value) => json_decode($value, true),
+        );
+    }
+
+    protected function monday(): Attribute
+    {
+        return $this->makeAttribute();
+    }
+
+    protected function thursday(): Attribute
+    {
+        return $this->makeAttribute();
+    }
+
+    protected function wednesday(): Attribute
+    {
+        return $this->makeAttribute();
+    }
+
+    protected function tuesday(): Attribute
+    {
+        return $this->makeAttribute();
+    }
+
+    protected function friday(): Attribute
+    {
+        return $this->makeAttribute();
+    }
+
+    protected function saturday(): Attribute
+    {
+        return $this->makeAttribute();
+    }
+
+    protected function sunday(): Attribute
+    {
+        return $this->makeAttribute();
+    }
 }
