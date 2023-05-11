d3b83c7fcf2c36c39db13bae28fb63231eca5478
Aurelien <aurelien.bouvaist@gmail.com>
2023-05-11 07:38:56 +0200

    new submit button

---
+++ resources/js/Pages/Admin/SubmitButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500" :disabled="disabled">
+        <slot></slot>
+    </button>
+</template>
+<script setup>
+defineProps({
+    disabled: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
