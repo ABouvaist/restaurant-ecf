8209fff69713645d4c40c3187635b467b5a5b355
Aurelien <aurelien.bouvaist@gmail.com>
2023-05-11 07:40:45 +0200

    page for creating a carte

---
+++ resources/js/Pages/Admin/RestaurantCarteCreate.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="overflow-scroll rounded-lg border border-gray-200 shadow-md m-5">
+        <div class="my-6">
+            <InputText v-model="form.name" name="Nom de la carte :" :error="form.errors.name" />
+        </div>
+
+        <SubmitButton :disabled="form.processing" @click="submit">Créer</SubmitButton>
+    </div>
+</template>
+
+<script setup>
+import {useForm} from "@inertiajs/vue3";
+import InputText from "@/Components/Inputs/InputText.vue";
+import SubmitButton from "@/Pages/Admin/SubmitButton.vue";
+
+const form = useForm({
+    name: '',
+})
+
+const submit = () => {
+    form.post(route('cartes.store'));
+}
+</script>
+
+<style scoped>
+
+</style>
