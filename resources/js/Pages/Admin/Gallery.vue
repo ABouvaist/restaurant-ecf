b47d40da40dff5c2c95bb40923d3e66cb8a3ad00
Aurelien <aurelien.bouvaist@gmail.com>
2023-05-18 17:59:53 +0200

    move timestamps columns at the end

--- resources/js/Pages/Admin/Gallery.vue
+++ resources/js/Pages/Admin/Gallery.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
+    <div class="overflow-scroll rounded-lg border border-gray-200 shadow-md m-5">
         <table class="w-full border-collapse bg-platinum text-left text-sm text-charcoal">
             <thead class="bg-gray-50">
                 <tr>
                     <th scope="col" class="px-6 py-4 font-medium text-gray-900">Titre</th>
                     <th scope="col" class="px-6 py-4 font-medium text-gray-900">Visible</th>
+                    <th scope="col" class="px-6 py-4 font-medium text-gray-900">Actions</th>
                     <th scope="col" class="px-6 py-4 font-medium text-gray-900">Création</th>
                     <th scope="col" class="px-6 py-4 font-medium text-gray-900">Mise a jour</th>
-                    <th scope="col" class="px-6 py-4 font-medium text-gray-900">Actions</th>
                 </tr>
             </thead>
             <tbody class="divide-y divide-gray-100 border-t border-gray-100">
@@ -16,11 +16,11 @@
                     <td class="px-6 py-4">
                         <Switch :value="image.shown" rounded @toggle="(shown) => update(image, shown)"/>
                     </td>
-                    <td class="px-6 py-4">{{ format(image.created_at) }}</td>
-                    <td class="px-6 py-4">{{ format(image.updated_at) }}</td>
                     <td class="px-6 py-4">
                         <Link :href="route('images.show', image.id)">modifier</Link>
                     </td>
+                    <td class="px-6 py-4">{{ format(image.created_at) }}</td>
+                    <td class="px-6 py-4">{{ format(image.updated_at) }}</td>
                 </tr>
 
             </tbody>
