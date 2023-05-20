5704e20439d0b62385c414bb7b81e5861fe1fb9f
Aurelien <aurelien.bouvaist@gmail.com>
2023-05-20 10:04:30 +0200

    small refactoring

--- resources/js/Components/Admin/AdminNav.vue
+++ resources/js/Components/Admin/AdminNav.vue
@@ -1,22 +1,38 @@
 <template>
     <nav class="pt-4">
         <ul class="flex justify-around">
-            <li>
-                <Link :href="route('images.index')">Gallerie</Link>
+            <li v-for="tab in tabs" :key="tab.id">
+                <Link :href="route(tab.route)">{{ tab.name }}</Link>
             </li>
-            <li>
-                <Link :href="route('cartes.index')">Carte</Link>
-            </li>
-            <li>
-                <Link :href="route('menus.index')">Menus</Link>
-            </li>
-            <li>Horaires</li>
         </ul>
     </nav>
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/vue3';
+
+const tabs = [
+    {
+        id: 'cartes_index',
+        name: 'Cartes',
+        route: 'cartes.index',
+    },
+    {
+        id: 'menus_index',
+        name: 'Menus',
+        route: 'menus.index'
+    },
+    {
+        id: 'gallery_index',
+        name: 'Gallerie',
+        route: 'images.index'
+    },
+    {
+        id: 'hours_index',
+        name: 'Horaires',
+        route: 'images.index'//TODO: change route to horaires.index when implemented
+    }
+]
 
 </script>
 
