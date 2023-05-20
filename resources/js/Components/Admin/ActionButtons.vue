15fc2fecae982623272bbdb31964ae5680320a63
Aurelien <aurelien.bouvaist@gmail.com>
2023-05-20 07:48:04 +0200

    component for easy reuse of submit & delete buttons in forms

---
+++ resources/js/Components/Admin/ActionButtons.vue
@@ -0,0 +1,17 @@
+<template>
+    <SubmitButton :disabled="disabled" @click="$emit('submit')">Enregistrer</SubmitButton>
+    <DeleteButton :disabled="disabled" @delete="$emit('delete')">Supprimer</DeleteButton>
+</template>
+<script setup>
+import SubmitButton from "@/Components/Admin/SubmitButton.vue";
+import DeleteButton from "@/Components/Admin/DeleteButton.vue";
+
+defineProps({
+    disabled: {
+        type: Boolean,
+        default: false
+    }
+})
+
+defineEmits(['submit', 'delete']);
+</script>
